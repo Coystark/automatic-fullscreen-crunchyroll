c663355c13a4b8ea2c167128ccc0516d18716bfd
Caio Henrique <caio_bnh22@hotmail.com>
2020-04-12 17:11:21 -0300

    fix

--- content.js
+++ content.js
@@ -1,23 +1,23 @@
-window.addEventListener('load', function () {
-	chrome.storage.sync.get(['activated'], ({ activated }) => {
+window.addEventListener("load", function () {
+	chrome.storage.sync.get(["activated"], ({ activated }) => {
 		if (activated == undefined) {
 			chrome.storage.sync.set({ activated: false });
 		}
 
 		if (activated) {
-			elem = document.querySelector('#vilos-player');
+			elem = document.querySelector("#vilos-player");
 
-			if (elem != null || elem != undefined) {
-				elem.style.position = 'fixed';
-				elem.style.left = '0';
-				elem.style.top = '0';
+			if (elem != null && elem != undefined) {
+				elem.style.position = "fixed";
+				elem.style.left = "0";
+				elem.style.top = "0";
 
 				// Need to hide all elements except #vilos-player
 				document
-					.querySelectorAll('body *:not( #vilos-player )')
+					.querySelectorAll("body *:not( #vilos-player )")
 					.forEach((value) => {
-						if (!value.querySelector('#vilos-player')) {
-							value.style.display = 'none';
+						if (!value.querySelector("#vilos-player")) {
+							value.style.display = "none";
 						}
 					});
 			}
